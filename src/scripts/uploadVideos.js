793302eeb62542c062e9e32cf257073e3eea872f
zen-pc <zennomi.kun@gmail.com>
2023-04-12 22:24:00 +0700

    upload hatnhan and luongtu course

--- src/scripts/uploadVideos.js
+++ src/scripts/uploadVideos.js
@@ -5,52 +5,60 @@ const Video = require("../models/video.model")
 const Course = require("../models/course.model")
 
 const inputs = [
-    {
-        file: "./data/daodongco.json",
-        name: "Dao Động Cơ"
-    },
-    {
-        file: "./data/songco.json",
-        name: "Sóng Cơ"
-    },
-    {
-        file: "./data/machlc.json",
-        name: "Mạch LC - Sóng điện từ"
-    },
-    {
-        file: "./data/dienxoaychieu.json",
-        name: "Điện Xoay Chiều"
-    },
-    {
-        file: "./data/songanhsang.json",
-        name: "Sóng Ánh Sáng"
-    },
+  {
+    file: "./data/daodongco.json",
+    name: "Dao Động Cơ"
+  },
+  {
+    file: "./data/songco.json",
+    name: "Sóng Cơ"
+  },
+  {
+    file: "./data/machlc.json",
+    name: "Mạch LC - Sóng điện từ"
+  },
+  {
+    file: "./data/dienxoaychieu.json",
+    name: "Điện Xoay Chiều"
+  },
+  {
+    file: "./data/songanhsang.json",
+    name: "Sóng Ánh Sáng"
+  },
+  {
+    file: "./data/luongtu.json",
+    name: "Lượng Tử Ánh Sáng"
+  },
+  {
+    file: "./data/hatnhan.json",
+    name: "Vật Lý Hạt Nhân"
+  }
 ]
-const input = inputs[2]
+const input = inputs[6]
 const data = require(input.file)
 const courseName = input.name;
 
 (async () => {
-    const videos = await Promise.all(data.map(video => {
-        return (() => {
-            return Video.create({
-                title: video.title,
-                url: video.url,
-                description: video.description,
-                isPublic: false,
-                grade: 12,
-            })
-        })()
-    }))
-    const course = await Course.create({
-        title: courseName,
+  const videos = await Promise.all(data.map(video => {
+    return (() => {
+      return Video.create({
+        title: video.title,
+        url: video.url,
+        description: video.description,
+        isPublic: true,
         grade: 12,
-        description: courseName,
-        coverURL: "https://i.imgur.com/4QKzSWl.jpeg",
-        isPublished: true,
-        isSale: false,
-        videos: videos.map((v, index) => ({ id: v._id, index })),
-        price: 200000
-    })
-    console.log("Done")
+      })
+    })()
+  }))
+  const course = await Course.create({
+    title: courseName,
+    grade: 12,
+    description: courseName,
+    coverURL: "https://i.imgur.com/4QKzSWl.jpeg",
+    isPublished: true,
+    isSale: false,
+    videos: videos.map((v, index) => ({ id: v._id, index })),
+    price: 200000
+  })
+  console.log("Done")
 })()
